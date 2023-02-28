4361bbf2ab00ccf9adaa1a5fd4737565ce355b6a
Justin Elue <mjuelue@gmail.com>
2023-02-28 11:38:45 -0600

    Stats

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament,alive) VALUES 
+('Mr. Chocolate', 20, 'F', 'black', 'chillin', 1 ),
+('Rowdy', 20, 'F', 'black', 'chillin', 1 ),
+('Tabitha', 20, 'F', 'black', 'chillin', 1 ),
+('Sergeant Brown', 20, 'F', 'black', 'chillin', 1 ),
+('Melissa', 20, 'M', 'black', 'chillin', 1 ),
+('Grinch', 20, 'M', 'green', 'schemin', 1 ),
+('Wendy', 20, 'M', 'black', 'chillin', 1 ),
+(NULL, 20, 'M', 'black', 'chillin', 0 )
